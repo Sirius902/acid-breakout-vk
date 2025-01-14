252e71ad143060704f9c32e0b70666051ed96a63
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-01-13 16:39:50 -0800

    fix(nix): zig hash for aarch64-darwin

--- nix/zig/default.nix
+++ nix/zig/default.nix
@@ -1,4 +1,7 @@
-{stdenv}: let
+{
+  lib,
+  stdenv,
+}: let
   throwSystem = throw "Unsupported system: ${stdenv.hostPlatform.system}";
 
   urlArch =
@@ -10,13 +13,24 @@
     }
     .${stdenv.hostPlatform.system}
     or throwSystem;
+
+  # TODO(Sirius902) Fill in other hashes as needed.
+  sha256 =
+    {
+      x86_64-linux = "sha256:033q435y14v50lp80yxi3llkvzryq6ldgzjcq39dlgisppq5zi9c";
+      aarch64-linux = lib.fakeSha256;
+      x86_64-darwin = lib.fakeSha256;
+      aarch64-darwin = "sha256:1z0i1bgr7s3h9svdf69dmsqmvdqnyd383n7v405qlpa8q4byxmab";
+    }
+    .${stdenv.hostPlatform.system}
+    or throwSystem;
 in
   stdenv.mkDerivation (finalAttrs: {
     pname = "zig";
     version = "0.13.0-dev.351+64ef45eb0";
     src = fetchTarball {
+      inherit sha256;
       url = "https://pkg.machengine.org/zig/zig-${urlArch}-${finalAttrs.version}.tar.xz";
-      sha256 = "sha256:033q435y14v50lp80yxi3llkvzryq6ldgzjcq39dlgisppq5zi9c";
     };
     dontConfigure = true;
     dontBuild = true;
